440c42351d580def901fee804ddf20035b936f85
Piergiorgio Yankah <piergiorgio.yankah@student.hogent.be>
2020-11-26 12:44:35 +0100

    Adding main classes for frontend
    Co-authored-by: inigoBereciartua <inigo.bereciartua@student.hogent.be>

---
+++ Persistence/CategoryRepository.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TravelListApp.Model;
+
+namespace TravelListApp.Persistence
+{
+    class CategoryRepository : Repository<Category>
+    {
+        public void addItem<Category>(Category item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Category> getAllItems<Category>()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void removeItem<Category>(int Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void updateItem<Category>(int Id, Category newObject)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Persistence/Repository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TravelListApp.Persistence
+{
+    interface Repository<T>
+    {
+        void addItem<T>(T item);
+        void removeItem<T>(int Id);
+        List<T> getAllItems<T>();
+        void updateItem<T>(int Id, T newObject);
+    }
+}
