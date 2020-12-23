c7946f64f49d3c21f1419439d5b491640e187c4d
Piergiorgio Yankah <piergiorgio.yankah@student.hogent.be>
2020-12-23 12:44:17 +0100

    Adding dao's objects
    Co-authored-by: Inigo Bereciartua <inigo.bereciartua@student.hogent.be>

---
+++ TravelListApp-Backend/Data/ApplicationDbContext.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TravelListApp_Backend.Data
+{
+    public class ApplicationDbContext
+    {
+    }
+}
