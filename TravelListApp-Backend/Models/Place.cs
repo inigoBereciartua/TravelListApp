f12a8e4ac1f0d03e46668f42b7e6464f440a054e
Piergiorgio Yankah <piergiorgio.yankah@student.hogent.be>
2020-12-26 12:28:48 +0100

    Places model implementation

--- TravelListApp-Backend/Models/Place.cs
+++ TravelListApp-Backend/Models/Place.cs
@@ -6,5 +6,33 @@ namespace TravelListApp_Backend.Models
 {
     public class Place
     {
+        #region Fields
+        private String _name;
+        private bool _visited;
+        #endregion Fields
+
+        #region Constructor
+
+        public Place(string name)
+        {
+            Name = name;
+            Visited = false;
+        }
+        #endregion Constructor
+
+        #region Properties
+        public String Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+
+        public bool Visited
+        {
+            get { return _visited; }
+            set { _visited = value; }
+        }
+        #endregion Properties
+
     }
 }
