daf29b3464c5a63f808fb1579d5f0a01b45fdcaa
Iñigo <inigo.bereciartua@student.hogent.be>
2020-12-26 11:48:08 +0100

    App running

--- NewFolder1/NavView.xaml.cs
+++ NewFolder1/NavView.xaml.cs
@@ -1,17 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Windows.UI.Xaml;
+﻿using System.Reflection;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
+using Windows.UI.Xaml.Media.Animation;
 
 // La plantilla de elemento Página en blanco está documentada en https://go.microsoft.com/fwlink/?LinkId=234238
 
@@ -22,9 +11,34 @@ namespace TravelListApp.NewFolder1
     /// </summary>
     public sealed partial class NavView : Page
     {
-        public NavView()
+
+        public NavView() => this.InitializeComponent();
+        private NavigationViewItem _lastItem;
+
+        private void NavigationView_OnItemInvoked(
+            NavigationView sender,
+            NavigationViewItemInvokedEventArgs args)
         {
-            this.InitializeComponent();
+            var item = args.InvokedItemContainer as NavigationViewItem;
+            if (item == null || item == _lastItem)
+                return;
+            var clickedView = item.Tag?.ToString();
+            if (!NavigateToView(clickedView)) return;
+            _lastItem = item;
         }
+        private bool NavigateToView(string clickedView)
+        {
+            var view = Assembly.GetExecutingAssembly()
+                .GetType($"NavigationView.Views.{clickedView}");
+
+            if (string.IsNullOrWhiteSpace(clickedView) || view == null)
+            {
+                return false;
+            }
+
+            ContentFrame.Navigate(view, null, new EntranceNavigationTransitionInfo());
+            return true;
+
+        }      
     }
 }
