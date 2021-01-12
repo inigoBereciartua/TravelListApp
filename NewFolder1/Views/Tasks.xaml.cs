8a01798a9df321071dca43038f847bcc6641a7e6
Iñigo <inigo.bereciartua@student.hogent.be>
2021-01-12 16:20:22 +0100

    Task view done

--- NewFolder1/Views/Tasks.xaml.cs
+++ NewFolder1/Views/Tasks.xaml.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using TravelListApp.Model;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
@@ -22,9 +24,57 @@ namespace TravelListApp.NewFolder1.Views
     /// </summary>
     public sealed partial class Tasks : Page
     {
+        public ObservableCollection<Task> TasksCollection { get; set; } = new ObservableCollection<Task>();
         public Tasks()
         {
             this.InitializeComponent();
+            TasksList.ItemsSource = TasksCollection;
+        }
+
+        private void CreateTask_Click(object sender, RoutedEventArgs e)
+        {
+            ErrorText.Text = "";
+            if (NewTaskName.Text == "")
+            {
+                ErrorText.Text = "Task's name can't be empty";
+            }
+            else if (NameOfTaskIsInUse(NewTaskName.Text))
+            {
+                ErrorText.Text = "That task name is already in use";
+            }
+            else
+            {
+                TasksCollection.Add(new Task { Name = NewTaskName.Text });
+                //TODO: Call backend to create Item
+            }
+        }
+
+        private bool NameOfTaskIsInUse(string text)
+        {
+            foreach(var task in TasksCollection)
+            {
+                if(task.Name == text)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        private void TasksList_ItemClick(object sender, ItemClickEventArgs e)
+        {
+
+        }
+
+        private async void AppBarButton_Click(object sender, RoutedEventArgs e)
+        {
+            var task = (sender as AppBarButton).DataContext;
+            ContentDialogResult result = await deleteTaskDialog.ShowAsync();
+            if (result == ContentDialogResult.Primary)
+            {
+                TasksCollection.Remove((Task)task);
+                //TODO: Call backend to delete Task
+            }
         }
     }
 }
