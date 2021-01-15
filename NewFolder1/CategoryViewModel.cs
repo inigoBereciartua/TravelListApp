a0d8d02532553fecc034f992639627788b7d8f82
Piergiorgio Yankah <piergiorgio.yankah@student.hogent.be>
2021-01-15 08:13:31 +0100

    Deleting repository

---
+++ NewFolder1/CategoryViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TravelListApp.NewFolder1
+{
+    class CategoryViewModel
+    {
+        public CategoryViewModel()
+        {
+
+        }
+
+        public string Category { get; set; }
+        public string CategoryName { get; set; }
+
+    }
+}
