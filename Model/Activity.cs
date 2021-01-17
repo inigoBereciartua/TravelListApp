6f1807bdd4e2343f9836339a49bacdb2d0244965
Iñigo <inigo.bereciartua@student.hogent.be>
2021-01-17 12:19:37 +0100

    Activity fix

---
+++ Model/Activity.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TravelListApp.Model
+{
+    public class Activity
+    {
+        public int Id { get; set; }
+        public string Description { get; set; }
+        public DateTimeOffset Start { get; set; }        
+    }
+}
