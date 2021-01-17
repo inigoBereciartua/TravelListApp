9e902e7e60af09361f2113b98fe118df4694255e
Piergiorgio Yankah <piergiorgio.yankah@student.hogent.be>
2021-01-17 03:25:16 +0100

    Fix

--- TravelListApp-Backend/DTO's/CategoryItem.cs
+++ TravelListApp-Backend/DTO's/CategoryItem.cs
@@ -5,7 +5,7 @@ using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace TravelListApp_Backend.Models
+namespace TravelListApp_Backend.DTO_s
 {
     public class CategoryItem
     {
