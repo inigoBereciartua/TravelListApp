65c97f4191e0a0ac8644da3e615846a3316038ee
Piergiorgio Yankah <piergiorgio.yankah@student.hogent.be>
2021-01-17 04:38:04 +0100

    Adding Api

---
+++ TravelListApp-Backend/DTO's/CategoryTaskDTO.cs
@@ -0,0 +1,19 @@
+﻿using NJsonSchema.Annotations;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TravelListApp_Backend.DTO_s
+{
+    public class CategoryTaskDTO
+    {
+        [Required]
+        [NotNull]
+        public int CategorylId { get; set; }
+        [Required]
+        [NotNull]
+        public int TaskId { get; set; }
+    }
+}
